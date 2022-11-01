7e88a6ea5893b811a9fb513046398606d1643f77
Raku Zeta <zetaraku@gmail.com>
2022-11-01 23:32:53 +0800

    📈 add Google tag

---
+++ public/gtag.js
@@ -0,0 +1,10 @@
+import 'https://www.googletagmanager.com/gtag/js?id=G-Z7W6385J02';
+
+window.dataLayer = window.dataLayer ?? [];
+
+function gtag() {
+  window.dataLayer.push(arguments);
+}
+
+gtag('js', new Date());
+gtag('config', 'G-Z7W6385J02');
